8a92088794d802055d7326b010349808d92ad877
ManveerSingh0 <singhcoder2005@gmail.com>
2026-01-29 16:23:16 +0530

    nothing

--- main.cpp
+++ main.cpp
@@ -1,9 +1,32 @@
+#include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
 
 
-
 int main(){
-  sf::Window window(sf::VideoMode({800,800}) , "2d_dodge_player");
+  sf::RenderWindow window(sf::VideoMode({800,800}) , "2d_dodge_player", sf::Style::Default);
+  window.setPosition({0,0});
+
+
+
+  auto on_close = [&window](sf::Event::Closed){
+    window.close();
+  };
+
+  auto close_on_escape = [&window](const sf::Event::KeyPressed& event){
+    if(event.scancode == sf::Keyboard::Scancode::Escape){
+      window.close();
+    }
+  };
+
+  // Main loop 
+  while(window.isOpen()){
+    window.handleEvents(on_close, close_on_escape);
+
+
+    window.clear(sf::Color::Red);
+    window.display();
+  }
+
 
   
 }
